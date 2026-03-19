ecd09d5d4ad6f880a8304293719d7b3e4ef4184d
Aaditya Bansal <137265000+CodeBreaker-0111@users.noreply.github.com>
2026-03-19 23:05:29 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,19 +1,18 @@
-async function  weather(){
-    let city=document.getElementById("search").value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ba439bd69e805343404955a719d8098f&units=metric`
-    
+async function weather() {
+    let city = document.getElementById("search").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1b85a14c50f987d93d0a1e99f64cd069&units=metric`;
+
     try {
         const response = await fetch(url);
         const result = await response.json();
         console.log(result);
-        document.getElementById("location_val").innerHTML=result.name;
-        document.getElementById("temp_val").innerHTML=Math.floor(result.main.temp);
-        document.getElementById("pre_val").innerHTML=result.main.pressure+" mbar";
-        document.getElementById("feels_like").innerHTML="Feels Like "+Math.floor(result.main.feels_like)+"&degc";        ;
-        document.getElementById("hum_val").innerHTML=result.main.humidity+" %";
-        document.getElementById("wind_val").innerHTML=result.wind.speed+" km/h";
+        document.getElementById("location_val").innerHTML = result.name;
+        document.getElementById("temp_val").innerHTML = Math.floor(result.main.temp);
+        document.getElementById("pre_val").innerHTML = result.main.pressure + " mbar";
+        document.getElementById("feels_like").innerHTML = "Feels Like " + Math.floor(result.main.feels_like) + "&degc";
+        document.getElementById("hum_val").innerHTML = result.main.humidity + " %";
+        document.getElementById("wind_val").innerHTML = result.wind.speed + " km/h";
     } catch (error) {
-        // console.error(error);
         alert("city not found");
     }
 }
